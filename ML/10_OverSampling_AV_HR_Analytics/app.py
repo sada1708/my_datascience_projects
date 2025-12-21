b7650410ed31d1967fb99d9712ace69d825e769e
Sada1708 <sada1708@gmail.com>
2025-12-21 15:01:50 +0530

    changes to app.py

--- ML/10_OverSampling_AV_HR_Analytics/app.py
+++ ML/10_OverSampling_AV_HR_Analytics/app.py
@@ -6,16 +6,13 @@ import joblib
 
 st.title("HR job Look Prediction")
 
-from pathlib import Path
-import pandas as pd
+#from pathlib import Path
+### This gets the absolute path of the directory where app.py lives
+###current_dir = Path(__file__).parent
+###file_path = current_dir / "train.csv"
+###df = pd.read_csv(file_path)
 
-# This gets the absolute path of the directory where app.py lives
-current_dir = Path(__file__).parent
-file_path = current_dir / "train.csv"
-
-df = pd.read_csv(file_path)
-
-#df = pd.read_csv('train.csv')
+df = pd.read_csv('train.csv')
 
 #input fields
 
